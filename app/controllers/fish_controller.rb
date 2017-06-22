f409f1b9b44a133387aecbe43b1ae5372db9484e
Cody Frazer <frazercr@gmail.com>
2017-06-21 23:17:17 -0400

    Fix controller structure around Creature index

--- app/controllers/fish_controller.rb
+++
@@ -1,5 +0,0 @@
-class FishController < ApplicationController
-  def index
-    @fish = Fish.all
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root "home#index"
 
-  get "/fish", to: "fish#index"
+  get "/creatures", to: "creatures#index"
   
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
